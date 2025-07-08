9f771122d46d07310d8e7f91f18d8d257cec1a6c
priyanshu0511 <priyanshu05112@gmail.com>
2025-07-08 22:01:29 +0530

    fixed backend wakeup issues

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,15 @@
 import { io } from "socket.io-client";
 
 export const initSocket = async () => {
+
+   const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+  try {
+    await fetch (BACKEND_URL,{cache:'no-store'})
+  } catch (error) {
+    throw error;
+  }
+
   const option = {
     "force new connection": true,
     reconnectionAttempts: Infinity,
