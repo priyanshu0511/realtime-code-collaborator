5d6f14f28c19c81cdd940169df4deb455ac83998
priyanshu0511 <priyanshu05112@gmail.com>
2025-07-14 23:22:43 +0530

    fixed backend issues

--- client/api/wakeup.js
+++ client/api/wakeup.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    await fetch(import.meta.env.VITE_BACKEND_URL, {
+    await fetch("https://realtime-collaborative-code-editor-8fu4.onrender.com/", {
       method: "GET",
     });
     res.status(200).json({ status: "Backend wake-up sent" });
